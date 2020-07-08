b83070e674e22fa68e30b1eaa9b60c333bcd22fd
andersonfds <andersonlivegja.as@gmail.com>
2020-07-08 04:39:26 -0300

    save point ex7

---
+++ Classes/Ex07/Book.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace POOP2.Classes.Ex07
+{
+    public class Book
+    {
+        private string author;
+        private string publisher;
+        private List<Chapter> chapters;
+
+        public string Author { get => author; set => author = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public List<Chapter> Chapters { get => chapters; set => chapters = value; }
+
+        public void Print()
+        {
+            MessageBox.Show(string.Format("Autor: {0}\nEditora: {1}\nCapitulos: {2}\nPáginas: {3}", Author, Publisher, Chapters.Count, PageCount()));
+        }
+
+        public void TurnChapter()
+        {
+            MessageBox.Show("Capitulo mudado com sucesso");
+        }
+
+        public int PageCount()
+        {
+            return chapters.Sum(x => x.PageCount);
+        }
+    }
+}
